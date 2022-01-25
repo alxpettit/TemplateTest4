f5b7d6269526db194c490d48779f2fc97d3c312e
Alexandria <28266167+alxpettit@users.noreply.github.com>
2022-01-25 11:06:18 -0800

    Initial commit

---
+++ utils/push.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# VERY IMPORTANT! Strict mode. See: http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+source project.conf
+source utils/check-port.sh
+
+ampy --port "$PORT" put "$FILE" "$@"
